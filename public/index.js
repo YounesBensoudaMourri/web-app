dca776e0444f32bb53a66975a38200ab1e409699
Ouassim Khebzegga <ouassimkhebzegga@DNa8193f6.SUNet>
2020-05-29 13:26:51 -0700

    add login html and css

---
+++ public/index.js
@@ -0,0 +1,16 @@
+const main = () => {
+
+    let signIn = document.getElementById("signInLink");
+    let create = document.getElementById("createLink");
+
+    signIn.addEventListener('click', (even) => {
+        document.getElementById("log").classList.add('hidden');
+        document.getElementById("create").classList.remove('hidden');
+    })
+    create.addEventListener('click', (even) => {
+        document.getElementById("log").classList.remove('hidden');
+        document.getElementById("create").classList.add('hidden');
+    })
+}
+
+main()
